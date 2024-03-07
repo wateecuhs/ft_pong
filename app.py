495b437ceb8189c853b7a0dfac78b5aece6a493c
Pierre ANGER <waticouzz@gmail.com>
2024-03-07 11:07:49 +0100

    forgot again oopsie

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask, render_template, send_from_directory, request, jsonify, Response
+from flask import Flask, render_template, send_from_directory, request, jsonify, Response, redirect,send_file
 import json
 import string
 import random
+import os
+import requests
 import datetime
 
 app = Flask(__name__)
 def id_generator():
 	return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+
+@app.route("/start_game", methods=['POST'])
+def start_game():
+	roomCode = request.args['roomCode']
+	with open('rooms.json', 'r') as file:
+		rooms = json.load(file)
+	for room in rooms:
+		if room['roomCode'] == roomCode and room['player1'] != None and room['player2'] != None:
+			room['status'] = "Playing"
+			with open('rooms.json', 'w') as file:
+				json.dump(rooms, file, sort_keys=True, indent='\t', separators=(',', ': '))
+			return jsonify({"message" : f"Game started in room {roomCode}"}), 200
+	return jsonify({"message" : "Couldn't start match"}), 404
+
+@app.route("/room_status", methods=['POST'])
+def room_status():
+	user = request.args['player']
+	with open('rooms.json', 'r') as file:
+		rooms = json.load(file)
+	for room in rooms:
+		if room['player1'] == user :
+			if (room['player2'] == None):
+				return jsonify({"status" : "Waiting for player 2"}), 409
+			else:
+				return jsonify({"status" : "Ready", "player" : room['player2']}), 200
 
 @app.route("/join_room", methods=['POST'])
 def join_room():
@@ -16,14 +43,19 @@ def join_room():
 		rooms = json.load(file)
 	for room in rooms:
 		if room['roomCode'] == roomCode:
+			if room['player1'] == user or room['player2'] == user:
+				if room['player1'] == user:
+					return jsonify({"message" : f"Player {user} joined room {roomCode}", "player1" : room['player1'], 'roomCode': roomCode}), 200
+				else:
+					return jsonify({"message" : f"Player {user} joined room {roomCode}", "player1" : room['player1'], 'roomCode': roomCode}), 200
 			if room['player2'] == None:
 				room['player2'] = user
 				with open('rooms.json', 'w') as file:
 					json.dump(rooms, file, sort_keys=True, indent='\t', separators=(',', ': '))
-				return jsonify({"message" : f"Player {user} joined room {roomCode}"}), 200
+				return jsonify({"message" : f"Player {user} joined room {roomCode}", "player1" : room['player1'], 'roomCode': roomCode}), 200
 			else:
-				return jsonify({"message" : f"Room {roomCode} is full"}), 409
-	return jsonify({"message" : f"Room {roomCode} not found"}), 404
+				return jsonify({"message" : f"Room {roomCode} is full", 'roomCode': roomCode}), 409
+	return jsonify({"message" : f"Room {roomCode} not found", 'roomCode': roomCode}), 404
 
 @app.route("/create_room", methods=['POST'])
 def create_room():
@@ -32,9 +64,9 @@ def create_room():
 		rooms = json.load(file)
 	for room in rooms:
 		if room['player1'] == user or room['player2'] == user:
-			return jsonify({"message" : f"Player already in a room ({room['roomCode']})"}), 409
+			return jsonify({"message" : f"Player already in a room ({room['roomCode']})", 'roomCode': room['roomCode']}), 409
 	roomCode = id_generator()
-	rooms.append({"roomCode": roomCode, "player1": user, "player2": None})
+	rooms.append({"roomCode": roomCode, "player1": user, "player2": None, "status": "Waiting for player 2"})
 	with open('rooms.json', 'w') as file:
 		json.dump(rooms, file, sort_keys=True, indent='\t', separators=(',', ': '))
 	return jsonify({"roomCode": roomCode}), 200
@@ -47,7 +79,16 @@ def get_stats():
 	for player in data:
 		if player['login'] == login:
 			return (jsonify(player['stats']['elo']))
-	return "Player not found!"
+	player = {
+		"login": login,
+		"stats": {
+			"elo": 500
+		}
+	}
+	data.append(player)
+	with open('database.json', 'w') as file:
+		json.dump(data, file, sort_keys=True, indent='\t', separators=(',', ': '))
+	return (jsonify(player['stats']['elo']))
 
 @app.route("/send_invite", methods=['POST'])
 def send_invite():
@@ -66,6 +107,36 @@ def send_invite():
 			return jsonify({"message": "Invite sent successfully"}), 200
 	else:
 		return jsonify({"message": "Player not found"}), 404
+	
+@app.route('/auth/42/callback', methods=['POST'])
+def auth_callback():
+	code = request.args.get('code')
+
+	token_url = 'https://api.intra.42.fr/oauth/token'
+	with open('API_DONT_PUSH.json', 'r') as file:
+		api = json.load(file)
+	client_id = api['client_id']
+	client_secret = api['client_secret']
+	redirect_uri = 'http://127.0.0.1:5000/'
+	params = {
+		'grant_type': 'authorization_code',
+		'client_id': client_id,
+		'client_secret': client_secret,
+		'code': code,
+		'redirect_uri': redirect_uri
+	}
+	response = requests.post(token_url, data=params)
+	print(response.json())
+	if response.ok:
+		access_token = response.json()['access_token']
+		response = requests.get('https://api.intra.42.fr/v2/me', headers={'Authorization': f'Bearer {access_token}'}).json()
+		return {"userId": response['login'], "image": response['image']['versions']['small']}, 200
+	else:
+		return 'Token exchange failed', 400
+
+@app.route('/match/<roomCode>', methods=['GET'])
+def match(roomCode):
+	return render_template("match.html", roomCode=roomCode)
 
 @app.route("/")
 def render_page():
@@ -76,4 +147,5 @@ def serve_file(filename):
 	return send_from_directory('.', filename)
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
--- js/index.js
+++ js/index.js
@@ -1,58 +1,7 @@
 var userId = undefined;
 var roomCode = undefined;
-
-function animateDots() {
-	const player1 = document.getElementById('player1_name');
-	const player2 = document.getElementById('player2_name');
-	player1.textContent = 'Player 1: You (' + userId + ')';
-	player2.textContent = 'Player 2: Waiting for someone to join';
-	let dots = '';
-	setInterval(() => {
-		if (dots.length === 3) {
-			dots = '';
-		} else {
-			dots += '.';
-		}
-		player2.textContent = 'Player 2: Waiting for someone to join' + dots;
-	}, 300);
-}
-
-function	fade_button(){
-	let button = document.getElementById('createRoom');
-	button.classList.add('shrinked');
-	let joinButton = document.getElementById('joinRoom');
-	joinButton.classList.add('shrinked');
-	setTimeout(function() {
-		button.remove();
-	}, 500);
-	roomCode = document.getElementById('roomcode');
-	roomCode.classList.add('created');
-	roomCode.disabled = true;
-	let copyButton = document.getElementById('copyRoomCode');
-	copyButton.classList.add('created');
-}
-
-function	fade_button_right(){
-	let button = document.getElementById('createRoom');
-	button.classList.add('shrinked');
-	let joinButton = document.getElementById('joinRoom');
-	joinButton.classList.add('shrinked');
-	setTimeout(function() {
-		button.remove();
-	}, 500);
-	roomCode = document.getElementById('roomcode');
-	roomCode.classList.add('created_right');
-	roomCode.disabled = true;
-	let copyButton = document.getElementById('copyRoomCode2');
-	copyButton.classList.add('created');
-}
-
-function	copyRoomCode(){
-	var	roomCode = document.getElementById('roomcode');
-	roomCode.select();
-	roomCode.setSelectionRange(0, 99999);
-	navigator.clipboard.writeText(roomCode.value);
-}
+var animateDotsBool = false;
+const roomError = document.getElementById('room_errors');
 
 function	tempLogin(){
 	userId = document.getElementById('player1').value;
@@ -68,9 +17,108 @@ function	tempLogin(){
 	})
 }
 
+function animateDots() {
+	const player1 = document.getElementById('player1_name');
+	const player2 = document.getElementById('player2_name');
+	var player1Elo = document.getElementById('elo').innerHTML;
+	player1.textContent = 'Player 1: You (' + player1Elo + ')';
+	player2.textContent = 'Player 2: Waiting for someone to join';
+	let dots = '';
+	setInterval(() => {
+		if (animateDotsBool) {
+			if (dots.length === 3) {
+				dots = '';
+			} else {
+				dots += '.';
+			}
+			player2.textContent = 'Player 2: Waiting for someone to join' + dots;
+		}
+	}, 300);
+}
+
+function	login(){
+	fetch(`https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-980c828f0609cae0f6762042edb5fa99953c2c0d899e807c79bca9e17c8db2bf&redirect_uri=http%3A%2F%2Fwateecuhs.pythonanywhere.com%2F&response_type=code`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json'
+	}})
+	.then(response => {
+		if (!response.ok) {
+			throw new Error('Error logging in');
+		}
+		return response.json();
+	}).then(data => {
+		console.log(data);
+	})
+}
+
+function	fade_button(){
+	let button = document.getElementById('createRoom');
+	button.classList.add('shrinked');
+	let joinButton = document.getElementById('joinRoom');
+	joinButton.classList.add('shrinked');
+	let roomCodeElem = document.getElementById('roomcode');
+	roomCodeElem.classList.add('created');
+	roomCodeElem.disabled = true;
+	let copyButton = document.getElementById('copyRoomCode');
+	copyButton.classList.add('created');
+}
+
+function	fade_button_right(){
+	let button = document.getElementById('createRoom');
+	button.classList.add('shrinked2');
+	let joinButton = document.getElementById('joinRoom');
+	joinButton.classList.add('shrinked2');
+	let roomCodeElem = document.getElementById('roomcode');
+	roomCodeElem.classList.add('created_right');
+	roomCodeElem.disabled = true;
+	let copyButton = document.getElementById('copyRoomCode2');
+	copyButton.classList.add('created');
+}
+
+function	copyRoomCode(){
+	let	roomCodeElem = document.getElementById('roomcode');
+	roomCodeElem.select();
+	roomCodeElem.setSelectionRange(0, 99999);
+	navigator.clipboard.writeText(roomCodeElem.value);
+}
+
+function	waitingForPlayer(){
+	fetch(`/room_status?player=${userId}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({userId})
+		})
+		.then(response => response.json())
+		.then(data => {
+			console.log(data);
+			if (data['status'] === 'Ready'){
+				animateDotsBool = false;
+				fetch(`/get_stats?player=${data['player']}`, {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify(data['player'])
+				})
+				.then(response => response.json())
+				.then(elo => {
+					document.getElementById('player2_name').textContent = 'Player 2: ' + data['player'] + ' (' + elo + ')';
+				})
+				clearTimeout(test);
+			}
+		})
+		.catch(error => {
+			console.log(error);
+		});
+		var test = setTimeout(waitingForPlayer, 1000);
+}
+
 function	joinRoom(){
 	if (userId === undefined){
-		alert('Please log in first');
+		roomError.innerHTML = 'Please log in first';
 		return;
 	}
 	roomCode = document.getElementById('roomcode').value;
@@ -83,24 +131,48 @@ function	joinRoom(){
 	})
 	.then(response => {
 		if (!response.ok) {
+			roomError.innerHTML = 'Error joining room';
 			throw new Error('Error joining room');
-        }
-        return response.json();
-    })
-    .then(data => {
+		}
+		return response.json();
+	})
+	.then(data => {
 		console.log(data);
-        document.getElementById('roomcode').value = roomCode;
+		document.getElementById('roomcode').value = roomCode;
 		fade_button_right();
-    })
-    .catch(error => {
+		const player1 = document.getElementById('player1_name');
+		const player2 = document.getElementById('player2_name');
+		var player2Elo = document.getElementById('elo').innerHTML;
+		fetch(`/get_stats?player=${data['player1']}`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data['player'])
+		})
+		.then(response => response.json())
+		.then(elo => {
+			console.log(elo);
+			player1.textContent = 'Player 1: ' + data['player1'] + ' (' + elo + ')';
+		})
+		if (data['player1'] === userId){
+			animateDotsBool = true;
+			animateDots();
+			waitingForPlayer();
+		}
+		else{
+			player2.textContent = 'Player 2: You (' + player2Elo + ')';
+		}
+		roomError.innerHTML = '';
+	})
+	.catch(error => {
 		console.error(error.message);
-        alert('Error creating room');
-    });	
+	});	
 }
 
 function	createRoom(){
 	if (userId === undefined){
-		alert('Please log in first');
+		roomError.innerHTML = 'Please log in first';
 		return;
 	}
 	fetch(`/create_room?player=${userId}`, {
@@ -110,25 +182,117 @@ function	createRoom(){
 		},
 		body: JSON.stringify({userId})
 	})
+	.then(async response => {
+		if (!response.ok) {
+			if (response.status === 409)
+			{
+				let data = await response.json();
+				console.log(data);
+				roomError.innerHTML = 'Error creating room: User already has a room (' + data['roomCode'] +')';
+			}
+			throw new Error('Error creating room');
+		}
+		return response.json();
+	})
+	.then(data => {
+		console.log(data);
+		document.getElementById('roomcode').value = data.roomCode;
+		fade_button();
+		animateDotsBool = true;
+		animateDots();
+		waitingForPlayer();
+		roomError.innerHTML = '';
+	})
+	.catch(error => {
+		console.error(error);
+	});	
+}
+
+function	startGame(){
+	if (userId === undefined){
+		roomError.innerHTML = 'Please log in first';
+		return;
+	}
+	console.log(roomCode);
+	if (roomCode === undefined){
+		roomError.innerHTML = 'Please create or join a room first';
+		return;
+	}
+	fetch(`/start_game?roomCode=${roomCode}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({userId})
+	})
 	.then(response => {
-        if (!response.ok) {
-            throw new Error('Error creating room');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-        document.getElementById('roomcode').value = data.roomCode;
-        fade_button();
-        animateDots();
-    })
-    .catch(error => {
-        console.error(error.message);
-        alert('Error creating room');
-    });	
+		if (!response.ok) {
+			throw new Error('Error starting game');
+		}
+		return response.json();
+	})
+	.then(data => {
+		console.log(data);
+
+	})
+	.catch(error => {
+		console.error(error);
+	});	
+}
+
+function getCodeFromUrl() {
+	const queryParams = new URLSearchParams(window.location.search);
+	const code = queryParams.get('code');
+	return code;
+}
+
+function	loginAuth(code){
+	fetch(`/auth/42/callback?code=${code}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({code})
+	}).then(response => {
+		if (!response.ok) {
+			throw new Error('Error logging in');
+		}
+		return response.json();
+	}).then(data => {
+		console.log(data);
+		userId = data['userId'];
+		document.getElementById('login').disabled = true;
+		document.getElementById('login').textContent = userId;
+		let img = document.createElement('img');
+		img.src = data['image'];
+		console.log(img.src);
+		img.alt = 'profile picture';
+		img.id = 'profile_pic';
+		img.classList.add('profile_pic');
+		document.getElementById('menu-item-container').appendChild(img);
+		fetch(`/get_stats?player=${userId}`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({userId})
+		})
+		.then(response => response.json())
+		.then(data => {
+			document.getElementById('elo').innerHTML = data;
+			roomError.innerHTML = '';
+		})
+	});
 }
 
 window.onload = function(){
+	document.getElementById('roomcode').value = null;
+	document.getElementById('roomcode').placeholder = 'ROOM CODE';
 	document.getElementById('login').addEventListener('click', tempLogin);
-	document.getElementById('roomcode').value = 'ROOM CODE';
+	const code = getCodeFromUrl();
+	console.log(code);
+	if (code !== null) {
+		loginAuth(code);
+	}
+	
 }
