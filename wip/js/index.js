bb325a55942e114f1611c8415060819328645010
Pierre <waticouzz@gmail.com>
2024-03-25 10:32:16 +0100

    new page progress

--- wip/js/index.js
+++ wip/js/index.js
@@ -1,6 +1,48 @@
-document.addEventListener("DOMContentLoaded", function() {
-	var navbar = document.getElementById('navbar');
-	setTimeout(function() {
-		navbar.style.top = '0';
-	}, 1000);
+var roomCode = undefined;
+
+function	copyCode(){
+	console.log('here');
+	let	roomCodeElem = document.getElementById('blocker-code');
+	roomCodeElem.select();
+	roomCodeElem.setSelectionRange(0, 99999);
+	navigator.clipboard.writeText(roomCodeElem.value);
+}
+
+function	isMatchValid() {
+	return true;
+}
+
+function	isRoomValid() {
+	return true;
+}
+
+function	player1Vote() {
+	leaveRoom();
+}
+
+function	player2Vote() {
+	leaveRoom();
+}
+
+function	createRoom(){
+	socket.emit('create',{"username": userId}, (response, code) => {
+		if (code !== 200){
+			roomError.innerHTML = response['message'];
+			return ;
+		}
+		roomCode = response['room_code']
+		displayRoomTab(roomCode);
+		roomError.innerHTML = "";
 	});
+}
+
+function	joinRoom() {
+	roomCode = document.getElementById('hidden-input').value;
+	if (isRoomValid(roomCode) === true) {
+		displayRoomTab(roomCode);
+	}
+}
+
+window.onload = function () {
+	document.getElementById('hidden-input').value = "";
+}
